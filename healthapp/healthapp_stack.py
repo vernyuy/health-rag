27cec267b121a42a68c65457ccb09a22d0045446
fonchu <fonchu.e.venyuy@gmail.com>
2024-12-01 06:55:27 +0100

    added rag with aws cdklabs, requires docker to run

--- healthapp/healthapp_stack.py
+++ healthapp/healthapp_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
-    aws_apigateway as _apigateway,
     aws_ec2 as ec2,    
     aws_iam as iam,
     aws_ssm as ssm,
@@ -10,6 +9,16 @@ from aws_cdk import (
     aws_ecr as ecr,
     aws_dynamodb as _dynamodb,
     Duration as Duration
+)
+from aws_cdk import (
+    CfnOutput,
+    aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
+    aws_apigateway as apigateway,
+)
+
+from cdklabs.generative_ai_cdk_constructs import (
+    bedrock
 )
 from constructs import Construct
 
@@ -38,6 +47,12 @@ class HealthappStack(Stack):
         #     )]
         # ))
 
+
+        kb = bedrock.KnowledgeBase(self, 'DocKnowledgeBase', 
+            embeddings_model= bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V1,                  
+        )
+
+        documentBucket = s3.Bucket(self, 'DocumentBucket')
         # Define IAM Role for Lambda
         lambda_role = iam.Role(
             self, "LambdaExecutionRole",
@@ -94,3 +109,76 @@ class HealthappStack(Stack):
                 handler=get_chats
             )
         )
+
+
+
+# from constructs import Construct
+
+# class HealthappStack(Stack):
+
+#     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+#         super().__init__(scope, construct_id, **kwargs)
+
+
+#         # kb = bedrock.KnowledgeBase(self, 'DocKnowledgeBase', 
+#         #     embeddings_model= bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V1,                  
+#         # )
+
+#         # documentBucket = s3.Bucket(self, 'DocumentBucket')
+
+#         deployment = s3deploy.BucketDeployment(self, "DeployDocuments",
+#             sources=[s3deploy.Source.asset("docs")],
+#             destination_bucket=documentBucket
+#         )
+
+#         bedrock.S3DataSource(self, 'KBS3DataSource',
+#             bucket= deployment.deployed_bucket,
+#             knowledge_base=kb,
+#             data_source_name='documents',
+#             chunking_strategy= bedrock.ChunkingStrategy.FIXED_SIZE,
+#             max_tokens=500,
+#             overlap_percentage=20   
+#         )
+
+#         kbQueryLambdaFunction = _lambda.Function(
+#             self, 'KBQueryFunction',
+#             runtime=_lambda.Runtime.PYTHON_3_12,
+#             code=_lambda.Code.from_asset('lambda'),
+#             handler='app.handler',
+#             environment={
+#                 'KB_ID': kb.knowledge_base_id,
+#                 'KB_MODEL_ARN': 'arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0',
+#             },
+#             timeout=Duration.seconds(15)
+#         )
+
+#         kbArn = f'arn:aws:bedrock:{Stack.of(self).region}:{Stack.of(self).account}:knowledge-base/{kb.knowledge_base_id}'
+
+#         policy_statement = iam.PolicyStatement(
+#             actions=[
+#                 "bedrock:Retrieve",
+#                 "bedrock:RetrieveAndGenerate",
+#                 "bedrock:InvokeModel"
+#             ],
+#             resources=[
+#                 "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
+#                 kbArn
+#             ]
+#         )
+
+#         kbQueryLambdaFunction.add_to_role_policy(policy_statement)
+
+#         api = apigateway.LambdaRestApi(
+#             self, 'KBQueryApiGW',
+#             handler=kbQueryLambdaFunction,
+#             proxy=False
+#         )
+
+#         kb_query = api.root.add_resource('query')
+#         kb_query.add_method('POST')
+
+#         CfnOutput(
+#             self, 'ApiEndpoint',
+#             value=api.url,
+#             description='The endpoint of the KB Query API'
+#         )
--- src/invoke_model_lambda.py
+++ src/invoke_model_lambda.py
@@ -1,162 +1,39 @@
 import boto3
 import json
+import os
 
-def lambda_handler(event, context):
-    print(event)
+def handler(event, context):
 
-    bedrock = boto3.client(
-        service_name='bedrock-runtime',
-        region_name='us-east-1'
+    input = event.get('body')
+    print("Question:", input)
+
+    client = boto3.client('bedrock-agent-runtime')
+
+    knowledgeBaseId = os.getenv("KB_ID")
+    modelArn = os.getenv("KB_MODEL_ARN")
+
+    resp = client.retrieve_and_generate(
+        input={
+            'text': input
+        },
+        retrieveAndGenerateConfiguration={
+            'knowledgeBaseConfiguration': {
+                'modelArn': modelArn,
+                'knowledgeBaseId': knowledgeBaseId,
+            },
+            'type': 'KNOWLEDGE_BASE'
+        }
     )
     
-    print(event)
-    slots = event['sessionState']['intent']['slots']
-    intent = event['sessionState']['intent']['name']
-    print(event['invocationSource'])
-    print(slots)
-    print(intent)
-    
-    input = {
-        "modelId": "anthropic.claude-v2",
-        "contentType": "application/json",
-        "accept": "*/*",
-        "body": "{\n  \"prompt\": \"\\n\\nHuman: story of two dogs\\n\\nAssistant:\",\n  \"max_tokens_to_sample\": 300\n}"
-    }
+    kbTextResponse = resp['output']['text']
+    print("KB response:", kbTextResponse)
 
-    response = bedrock.invoke_model(
-        body=input["body"],
-        modelId=input["modelId"],
-        accept=input["accept"],
-        contentType=input["contentType"]
-    )
-
-    response_body = json.loads(response['body'].read())
-    print("Model Response")
-    print(response_body['completion'])
     response = {
-                "sessionState": {
-                    "dialogAction": {
-                        'slotToElicit':'help',
-                        "type": "ElicitSlot"
-                    },
-                    "intent": {
-                        'name':intent,
-                        'slots': slots
-                        
-                        }
-                }
+        'statusCode': 200,
+        'body': json.dumps({
+            "question": event.get('body'),
+            "response": kbTextResponse,
+        }),
     }
+
     return response
-
-    
-
-
-# def lambda_handler(event, context):
-    
-#     print(event)
-#     slots = event['sessionState']['intent']['slots']
-#     intent = event['sessionState']['intent']['name']
-#     print(event['invocationSource'])
-#     print(slots)
-#     print(intent)
-    
-#     response = {
-#                 "sessionState": {
-#                     "dialogAction": {
-#                         'slotToElicit':'help',
-#                         "type": "ElicitSlot"
-#                     },
-#                     "intent": {
-#                         'name':intent,
-#                         'slots': slots
-                        
-#                         }
-#                 }
-#     }
-#     return response
-    
-#     # validation_result = validate(event['sessionState']['intent']['slots'])
-    
-#     # if event['invocationSource'] == 'DialogCodeHook':
-#     #     if not validation_result['isValid']:
-            
-#     #         if 'message' in validation_result:
-            
-#                 # response = {
-#                 # "sessionState": {
-#                 #     "dialogAction": {
-#                 #         'slotToElicit':validation_result['violatedSlot'],
-#                 #         "type": "ElicitSlot"
-#                 #     },
-#                 #     "intent": {
-#                 #         'name':intent,
-#                 #         'slots': slots
-                        
-#                 #         }
-#                 # },
-#     #             "messages": [
-#     #                 {
-#     #                     "contentType": "PlainText",
-#     #                     "content": validation_result['message']
-#     #                 }
-#     #             ]
-#     #           } 
-#     #         else:
-#     #             response = {
-#     #             "sessionState": {
-#     #                 "dialogAction": {
-#     #                     'slotToElicit':validation_result['violatedSlot'],
-#     #                     "type": "ElicitSlot"
-#     #                 },
-#     #                 "intent": {
-#     #                     'name':intent,
-#     #                     'slots': slots
-                        
-#     #                     }
-#     #             }
-#     #           } 
-    
-#     #         return response
-           
-#     #     else:
-#     #         response = {
-#     #         "sessionState": {
-#     #             "dialogAction": {
-#     #                 "type": "Delegate"
-#     #             },
-#     #             "intent": {
-#     #                 'name':intent,
-#     #                 'slots': slots
-                    
-#     #                 }
-        
-#     #         }
-#     #     }
-#     #         return response
-    
-#     # if event['invocationSource'] == 'FulfillmentCodeHook':
-        
-#     #     # Add order in Database
-        
-#     #     response = {
-#     #     "sessionState": {
-#     #         "dialogAction": {
-#     #             "type": "Close"
-#     #         },
-#     #         "intent": {
-#     #             'name':intent,
-#     #             'slots': slots,
-#     #             'state':'Fulfilled'
-                
-#     #             }
-    
-#     #     },
-#     #     "messages": [
-#     #         {
-#     #             "contentType": "PlainText",
-#     #             "content": "Thanks, I have placed your reservation"
-#     #         }
-#     #     ]
-#     # }
-            
-#     #     return response
